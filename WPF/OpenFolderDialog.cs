46ae6a52b1d768957618971f387fb66867e990a2
Lixfeld <thealphaskill@web.de>
2020-08-08 19:39:39 +0200

    Add OpenFolderDialog

---
+++ WPF/OpenFolderDialog.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Windows;
+
+namespace PROJECT.Dialogs
+{
+    public class OpenFolderDialog
+    {
+        // WindowsAPICodePack-Shell: https://www.nuget.org/packages/WindowsAPICodePack-Shell/
+        private readonly CommonOpenFileDialog openFolderDialog;
+        public string FolderName { get; private set; }
+
+        public OpenFolderDialog()
+        {
+            openFolderDialog = new CommonOpenFileDialog() { IsFolderPicker = true };
+        }
+
+        public bool? ShowDialog(Window owner)
+        {
+            CommonFileDialogResult dialogResult;
+            if (owner != null)
+                dialogResult = openFolderDialog.ShowDialog(owner);
+            else
+                dialogResult = openFolderDialog.ShowDialog();
+
+            switch (dialogResult)
+            {
+                case CommonFileDialogResult.Ok:
+                    FolderName = openFolderDialog.FileName;
+                    return true;
+                case CommonFileDialogResult.Cancel:
+                    return false;
+                case CommonFileDialogResult.None:
+                default:
+                    return null;
+            }
+        }
+    }
+}
